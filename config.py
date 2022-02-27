b7f323e48c20db7a21ca3bd946cc526f6cf831d5
faithremo <faithmakumi@gmail.com>
2022-02-27 21:32:16 +0300

    add a staging,development and testing classes

--- config.py
+++ config.py
@@ -10,3 +10,13 @@ class ProductionConfig(Config):
     DEBUG = False
     MAIL_DEBUG = False
     
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    
+class TestingConfig(Config):
+    TESTING = True
